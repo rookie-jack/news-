b204df90a01fc9d5f69933499842520239122fcc
qinchangjie <1028569103>
2020-11-16 11:57:26 +0800

    添加登录和注册单文件组件和登录框单文件子组件

---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>登录页面</div>
+    <my-input></my-input>
+  </div>
+</template>
+
+<script>
+import MyInput from "./MyInput.vue";
+export default {
+  components: {
+    MyInput,
+  },
+};
+</script>
+
+<style>
+</style>
