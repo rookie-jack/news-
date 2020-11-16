819b7425e43a1a1c7de71b6619fd491c1136e90b
qinchangjie <1028569103>
2020-11-16 15:07:48 +0800

    父子参数传递

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <div>登录页面</div>
-    <my-input></my-input>
+    <my-input tixing="请输入用户名" @sendVal="logVal"></my-input>
+    <my-input tixing="请输入密码" @sendVal="logVal"></my-input>
+    <my-input tixing="请输入昵称" @sendVal="logVal"></my-input>
   </div>
 </template>
 
 <script>
-import MyInput from "./MyInput.vue";
+import MyInput from "../components/MyInput";
 export default {
   components: {
     MyInput,
+  },
+  methods: {
+    logVal(val) {
+      console.log(val);
+    },
   },
 };
 </script>
