53c5bf7500cd2543c0cea608702269c18b9fb66a
qinchangjie <1028569103>
2020-11-20 20:33:26 +0800

    编辑用户信息页面样式布局完成

---
+++ src/views/profile/user.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="user">
+        <img
+          v-if="userInfo.head_img"
+          :src="'http://157.122.54.189:9083' + userInfo.head_img"
+          class="avatar"
+        />
+        <img v-else src="../../assets/logo.png" alt="" class="avatar" />
+        <div class="userright">
+          <div class="username">
+            <span
+              class="iconfont"
+              :class="{
+                iconxingbienan: userInfo.gender === 1,
+                iconxingbienv: userInfo.gender === 0,
+              }"
+            ></span>
+            {{ userInfo.nickname || userInfo.username }}
+          </div>
+          <div class="date">2020-12-10</div>
+        </div>
+        <span class="iconfont iconjiantou1"></span>
+      </div>
+      <div class="tool">
+        <UserBar
+          @click.native="logText"
+          :title="'我的关注'"
+          :name="'关注的用户'"
+        />
+        <UserBar title="我的跟帖" name="跟帖/回复" />
+        <UserBar title="我的收藏" name="文章/视频" />
+        <UserBar title="设置" />
+        <UserBar title="退出" @click.native="logout" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserBar from "../../components/UserBar.vue";
+export default {
+  components: { UserBar },
+  data() {
+    return {
+      userInfo: {},
+    };
+  },
+  created() {
+    this.$axios({
+      url: "http://157.122.54.189:9083/user/" + localStorage.getItem("userId"),
+      headers: {
+        Authorization: localStorage.getItem("token"),
+      },
+    }).then((res) => {
+      console.log(res);
+      const { message, data } = res.data;
+      if (message == "获取成功") {
+        this.userInfo = data;
+        console.log(this.userInfo);
+      }
+    });
+  },
+  methods: {
+    logout() {
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      this.$toast("退出成功");
+      this.$router.replace("/login");
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 100vw;
+  height: 600 /360 * 100vw;
+  background-color: #f0f0f0;
+
+  .user {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+    height: 80 /360 * 100vw;
+    padding: 20 /360 * 100vw 40 /360 * 100vw 20 /360 * 100vw;
+    border-bottom: 10 /360 * 100vw solid #ccc;
+    cursor: pointer;
+    .avatar {
+      width: 70 /360 * 100vw;
+      height: 70 /360 * 100vw;
+      border-radius: 50%;
+    }
+    .userright {
+      .username {
+        font-size: 18 /360 * 100vw;
+        .iconxingbienan {
+          color: #6cf;
+        }
+        .iconxingbienv {
+          color: #f63bc0;
+        }
+      }
+      .date {
+        font-size: 16 /360 * 100vw;
+        font-weight: 700;
+        color: #aaa;
+      }
+    }
+    .iconjiantou1 {
+      margin-right: 80 /360 * 100vw;
+      margin-top: 10 /360 * 100vw;
+      font-size: 24 /360 * 100vw;
+    }
+  }
+}
+</style>
---
+++ src/views/profile/useredit.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="header">
+        <span class="iconfont iconjiantou2"></span>
+        <b>编辑资料</b>
+      </div>
+      <div class="user">
+        <img src="../../assets/logo.png" alt="" class="avatar" />
+      </div>
+      <div class="tool">
+        <UserBar title="昵称" name="沙雕网友" />
+        <UserBar title="密码" name="******" />
+        <UserBar title="性别" name="男" />
+        <user-bar></user-bar>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserBar from "../../components/UserBar.vue";
+export default {
+  components: { UserBar },
+};
+</script>
+
+<style lang="less" scoped>
+.container {
+  width: 100vw;
+  height: 600 /360 * 100vw;
+  background-color: #f0f0f0;
+  text-align: center;
+
+  .header {
+    height: 50 /360 * 100vw;
+    line-height: 50 /360 * 100vw;
+    .iconjiantou2 {
+      float: left;
+      margin-left: 10 /360 * 100vw;
+      font-size: 20 /360 * 100vw;
+    }
+    b {
+      margin-left: -20 /360 * 100vw;
+      font-size: 16 /360 * 100vw;
+    }
+  }
+
+  .user {
+    .avatar {
+      margin: 30 /360 * 100vw 0;
+      width: 65 /360 * 100vw;
+      border-radius: 50%;
+    }
+  }
+}
+</style>
