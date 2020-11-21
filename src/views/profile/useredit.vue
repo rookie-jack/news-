b03ec54dfe2d5dcdff01d66cb24d31296fbb68e5
qinchangjie <1028569103>
2020-11-21 18:04:26 +0800

    修改用户昵称功能完成

--- src/views/profile/useredit.vue
+++ src/views/profile/useredit.vue
@@ -12,9 +12,21 @@
         <img v-else src="../../assets/logo.png" alt="" class="avatar" />
       </div>
       <div class="tool">
-        <UserBar title="昵称" :name="userInfo.nickname" />
+        <UserBar
+          title="昵称"
+          :name="userInfo.nickname"
+          @click.native="isshow = true"
+        />
         <UserBar title="密码" name="******" />
         <UserBar title="性别" :name="userInfo.gender == 1 ? '雄性' : '雌性'" />
+        <van-dialog
+          v-model="isshow"
+          show-cancel-button
+          title="修改昵称"
+          @confirm="setValue"
+        >
+          <van-field v-model="value" placeholder="请输入昵称" />
+        </van-dialog>
       </div>
     </div>
   </div>
@@ -28,22 +40,57 @@ export default {
   data() {
     return {
       userInfo: {},
+      isshow: false,
+      value: "",
     };
   },
   created() {
-    this.$axios({
-      url: "/user/" + localStorage.getItem("userId"),
-      headers: {
-        Authorization: localStorage.getItem("token"),
-      },
-    }).then((res) => {
-      console.log(res);
-      const { message, data } = res.data;
-      if (message == "获取成功") {
-        this.userInfo = data;
-        console.log(this.userInfo);
-      }
-    });
+    // this.$axios({
+    //   url: "/user/" + localStorage.getItem("userId"),
+    //   headers: {
+    //     Authorization: localStorage.getItem("token"),
+    //   },
+    // }).then((res) => {
+    //   console.log(res);
+    //   const { message, data } = res.data;
+    //   if (message == "获取成功") {
+    //     this.userInfo = data;
+    //     console.log(this.userInfo);
+    //   }
+    // });
+    this.loadPage();
+  },
+  methods: {
+    loadPage() {
+      this.$axios({
+        url: "/user/" + localStorage.getItem("userId"),
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+      }).then((res) => {
+        console.log(res);
+        const { message, data } = res.data;
+        if (message == "获取成功") {
+          this.userInfo = data;
+        }
+      });
+    },
+    setValue() {
+      console.log(this.value);
+      this.$axios({
+        method: "post",
+        url: "/user_update/" + localStorage.getItem("userId"),
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+        data: {
+          nickname: this.value,
+        },
+      }).then((res) => {
+        console.log(res.data);
+        this.loadPage();
+      });
+    },
   },
 };
 </script>
@@ -60,6 +107,7 @@ export default {
       margin: 30 /360 * 100vw 0;
       width: 65 /360 * 100vw;
       border-radius: 50%;
+      object-fit: cover;
     }
   }
 }
