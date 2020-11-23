d22e2f5f556f3f3853baae730362f201db8be690
qinchangjie <1028569103>
2020-11-23 15:21:06 +0800

    修改密码和修改性别完成

--- src/views/profile/useredit.vue
+++ src/views/profile/useredit.vue
@@ -17,16 +17,44 @@
           :name="userInfo.nickname"
           @click.native="isshow = true"
         />
-        <UserBar title="密码" name="******" />
-        <UserBar title="性别" :name="userInfo.gender == 1 ? '雄性' : '雌性'" />
+        <UserBar
+          title="密码"
+          name="******"
+          @click.native="isshowPassword = true"
+        />
+        <UserBar
+          title="性别"
+          :name="userInfo.gender == 1 ? '雄性' : '雌性'"
+          @click.native="isshowGender = true"
+        />
+        <!-- 输入框组件 -->
         <van-dialog
           v-model="isshow"
           show-cancel-button
           title="修改昵称"
-          @confirm="setValue"
+          @confirm="setnewNickname"
         >
-          <van-field v-model="value" placeholder="请输入昵称" />
+          <van-field v-model="newNickname" placeholder="请输入昵称" />
         </van-dialog>
+        <!-- 密码输入框组件 -->
+        <van-dialog
+          v-model="isshowPassword"
+          show-cancel-button
+          title="修改密码"
+          @confirm="setPassword"
+        >
+          <van-field
+            type="password"
+            v-model="newPassword"
+            placeholder="请输入昵称"
+          />
+        </van-dialog>
+        <!-- 性别选项组件引用 -->
+        <van-action-sheet
+          v-model="isshowGender"
+          :actions="actions"
+          @select="setGender"
+        />
       </div>
     </div>
   </div>
@@ -41,26 +69,21 @@ export default {
     return {
       userInfo: {},
       isshow: false,
-      value: "",
+      isshowPassword: false,
+      newNickname: "",
+      newPassword: "",
+      isshowGender: false,
+      actions: [
+        { name: "雄性", gender: 1 },
+        { name: "雌性", gender: 0 },
+      ],
     };
   },
   created() {
-    // this.$axios({
-    //   url: "/user/" + localStorage.getItem("userId"),
-    //   headers: {
-    //     Authorization: localStorage.getItem("token"),
-    //   },
-    // }).then((res) => {
-    //   console.log(res);
-    //   const { message, data } = res.data;
-    //   if (message == "获取成功") {
-    //     this.userInfo = data;
-    //     console.log(this.userInfo);
-    //   }
-    // });
     this.loadPage();
   },
   methods: {
+    // 编辑用户的请求,渲染页面
     loadPage() {
       this.$axios({
         url: "/user/" + localStorage.getItem("userId"),
@@ -75,8 +98,9 @@ export default {
         }
       });
     },
-    setValue() {
-      console.log(this.value);
+    // 修改用户名的请求
+    setnewNickname() {
+      console.log(this.newNickname);
       this.$axios({
         method: "post",
         url: "/user_update/" + localStorage.getItem("userId"),
@@ -84,10 +108,42 @@ export default {
           Authorization: localStorage.getItem("token"),
         },
         data: {
-          nickname: this.value,
+          nickname: this.newNickname,
         },
       }).then((res) => {
         console.log(res.data);
+        this.loadPage();
+      });
+    },
+    // 修改密码的请求
+    setPassword() {
+      this.$axios({
+        method: "post",
+        url: "/user_update/" + localStorage.getItem("userId"),
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+        data: {
+          password: this.newPassword,
+        },
+      }).then((res) => {
+        console.log(res.data);
+        this.loadPage();
+      });
+    },
+    setGender(item) {
+      this.$axios({
+        method: "post",
+        url: "/user_update/" + localStorage.getItem("userId"),
+        headers: {
+          Authorization: localStorage.getItem("token"),
+        },
+        data: {
+          gender: item.gender,
+        },
+      }).then((res) => {
+        console.log(res.data);
+        this.isshowGender = false;
         this.loadPage();
       });
     },
