d8004e9318f9f8257048c36cdae48092dba3a43d
qinchangjie <1028569103>
2020-11-24 16:00:40 +0800

    首页部分渲染完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
     <HomeHeader />
+    <van-tabs v-model="activeCategoryIndex">
+      <van-tab
+        v-for="category in categoryList"
+        :key="category.id"
+        :title="category.name"
+        >{{ category.name }}</van-tab
+      >
+    </van-tabs>
   </div>
 </template>
 
@@ -8,9 +16,26 @@
 import HomeHeader from "../components/HomeHeader.vue";
 export default {
   components: { HomeHeader },
+  data() {
+    return {
+      activeCategoryIndex: 0,
+      categoryList: [],
+    };
+  },
   methods: {},
+  created() {
+    this.$axios({
+      url: "/category",
+    }).then((res) => {
+      console.log(res);
+      this.categoryList = res.data.data;
+    });
+  },
 };
 </script>
 
 <style lang="less" scoped>
+\deep\ .van-tab {
+  font-size: 16 /360 * 100vw;
+}
 </style>
