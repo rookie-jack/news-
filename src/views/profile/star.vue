89a705fa2cd05871ed77a68c84e4a0307fccfd35
qinchangjie <1028569103>
2020-11-27 14:48:52 +0800

    无

---
+++ src/views/profile/star.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    收藏页
+    <TopNav title="我的收藏" />
+    <PostItem :PostData="post" v-for="post in postList" :key="post.id" />
+  </div>
+</template>
+
+<script>
+import PostItem from "../../components/PostItem.vue";
+import TopNav from "../../components/TopNav.vue";
+export default {
+  components: { TopNav, PostItem },
+  data() {
+    return {
+      postList: [],
+    };
+  },
+  created() {
+    this.$axios({
+      url: "/user_star",
+    }).then((res) => {
+      console.log(res.data.data);
+      this.postList = res.data.data;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
